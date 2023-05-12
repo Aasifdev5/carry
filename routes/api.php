dcc7382a7af900333c9dd7020a719185ac0573c5
Aasifdev5 <asifarslaan@gmail.com>
2023-05-12 16:12:55 +0530

    API login Completed

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/', [APIController::class, 'sign_in']);
+Route::post('/sign_in', [APIController::class, 'sign_in']);
